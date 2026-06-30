4b7b6d55c39f17682b77225454fc86efe3cacdcb
Théo Del Borrello <theo.del_borrello@etu.minesparis.psl.eu>
2026-06-30 11:30:11 +0200

    emplecement code

--- src/stark_fibonacci/main.py
+++ src/stark_fibonacci/main.py
@@ -1,2 +1,13 @@
-def main():
-    print("Hello World!")
+import numpy as np
+from numpy import galois
+
+prime=17
+GF = np.galois.GF(prime)
+
+a=GF([])
+def vec1023(u0,u1,n):
+    a.append(u0,u1)
+    for k in range(n):
+        a.append(a[-1]+a[-2])
+    print(a)
+vec1023(0,1,35)
